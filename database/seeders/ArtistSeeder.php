665acc37cb789475c3ea40a9c4f02075f655ce9d
Boniver Serge <serge.boniver@gmail.com>
2023-01-27 15:06:43 +0100

    Modèle Artist + migration + seeders

--- database/seeders/ArtistSeeder.php
+++ database/seeders/ArtistSeeder.php
@@ -21,7 +21,19 @@ class ArtistSeeder extends Seeder
         Artist::truncate();
 
         $artists = [
-            ['firstname'=>'Bob','lastname'=>'Sull'],
+            ['firstname'=>'Daniel','lastname'=>'Marcelin'],
+            ['firstname'=>'Philippe','lastname'=>'Laurent'],
+            ['firstname'=>'Marius','lastname'=>'Von Mayenburg'],
+            ['firstname'=>'Olivier','lastname'=>'Boudon'],
+            ['firstname'=>'Anne','lastname'=>'Loop'],
+            ['firstname'=>'Pietro','lastname'=>'Varasso'],
+            ['firstname'=>'Laurent','lastname'=>'Caron'],
+            ['firstname'=>'Elena','lastname'=>'Perez'],
+            ['firstname'=>'Guillaume','lastname'=>'Alexandre'],
+            ['firstname'=>'Claude ','lastname'=>'Semal'],
+            ['firstname'=>'Laurence','lastname'=>'Warin'],
         ];
+        //Insert data in the table
+        DB::table('artists')->insert($artists);
     }
 }
