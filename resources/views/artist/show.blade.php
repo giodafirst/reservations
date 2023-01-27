9e01e38b49256a8eb82ccc8f611e52ad3329c547
Boniver Serge <serge.boniver@gmail.com>
2023-01-27 17:22:20 +0100

    Form insert Artist

--- resources/views/artist/show.blade.php
+++ resources/views/artist/show.blade.php
@@ -4,5 +4,15 @@
 
 @section('content')
     <h1>{{ $artist->firstname }}{{ $artist->lastname }}</h1>
+
+    {{--<h2>Liste des types</h2>--}
+    <ul>
+    {{--@foreach($artist->types as $type)--}}
+    {{--    <li>{{ $type->type }}</li>--}}
+    {{--@endforeach--}}
+    </ul>
+
+    <div><a href="{{ route('artist.edit', $artist->id) }}">Modifier</a></div>
+
     <nav><a href="{{ route('artist.index') }}">Retour à l'index</a></nav>
 @endsection
