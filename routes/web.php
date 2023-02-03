d54e196723049a3e1c281a2461d436ded972a0b9
Boniver Serge <serge.boniver@gmail.com>
2023-02-03 16:36:30 +0100

    Breeze correction regression

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ArtistController;
-use App\Http\Controllers\TypeController;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,6 +19,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 Route::get('/type',[TypeController::class, 'index'])->name('type.index');
 Route::get('/type/{id}',[TypeController::class, 'show'])->name('type.show');
 Route::get('/artist',[ArtistController::class, 'index'])->name('artist.index');
@@ -29,5 +38,4 @@ Route::delete('/artist/{id}',[ArtistController::class, 'destroy'])->where('id','
 Route::get('/artist/create',[ArtistController::class, 'create'])->name('artist.create');
 Route::post('/artist',[ArtistController::class, 'store'])->name('artist.store');
 
-
-
+require __DIR__.'/auth.php';
