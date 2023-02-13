9377b4d0f05beda63fe7ae7151e61fb87929b181
Boniver Serge <serge.boniver@gmail.com>
2023-02-13 18:18:45 +0100

    Templates Show + representations list

--- resources/views/show/index.blade.php
+++ resources/views/show/index.blade.php
@@ -8,9 +8,17 @@
     <ul>
             @foreach($shows as $show)
                 <li><a href="{{ route('show.show', $show->id) }}">{{ $show->title }}</a>
-                @if($show->bookable)
-                    <span>{{ $show->price }}€</span>
-                @endif
+                    @if($show->bookable)
+                        <span>{{ $show->price }}€</span>
+                    @endif
+
+                    @if($show->representations->count()==1)
+                    - <span>1 représentation</span>
+                    @elseif($show->representations->count()>1)
+                    - <span>{{ $show->representations->count() }} représentations</span>
+                    @else
+                    - <em>aucune représentation</em>
+                    @endif
                 </li>
             @endforeach
     </ul>
