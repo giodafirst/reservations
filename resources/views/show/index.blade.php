6afb75016709c2da0b6e4672043daf9559b439ca
Ismail T <takkalismael@gmail.com>
2023-03-21 19:57:54 +0100

    sortby() method optimized

--- resources/views/show/index.blade.php
+++ resources/views/show/index.blade.php
@@ -1,7 +1,4 @@
-
-
 @extends('layouts.main')
-
 @section('title','Liste des spectacles')
 
 @section('content')
@@ -11,46 +8,66 @@
     <form action="{{ route('show.search') }}" method="GET">
         <input type="text" name="query" placeholder="{{ __('Search') }}...">
         <input type="date" name="date">
-        <label for="sortBy">{{ __('Sort') }} : </label>
-        <select name="sortBy" id="sortBy">
-            <option value="shows.title" selected>A->Z</option>
-            <option value="shows.location_id">{{ __('Locality') }}</option>
-            <option value="shows.bookable">{{ __('Bookable') }}</option>
-            <option value="price">{{ __('Price') }}</option>
-        </select>
         <label for="sort">{{ __('ByLocality') }} :</label>
         <select name="postal_code" id="sort">
-            <option value="none" selected>-----</option>
-            @foreach($localities as $locality)
-                <option value="{{$locality->postal_code}}">{{$locality->postal_code}}</option>
-            @endforeach
+        <option value="none" selected>-----</option>
+        @foreach($localities as $locality)
+            <option value="{{$locality->postal_code}}">{{$locality->postal_code}}</option>
+        @endforeach
         </select>
-
         <input type="checkbox" name="reservable" id="reservable">
         <label for="reservable">{{ __('Bookable') }} |</label>
-
         <button type="submit">{{ __('Search') }}</button>
     </form>
+    <!--Sort -->
+    <div class="navbar navbar-expand-lg navbar-light bg-light">
+        <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img alt="" title="" src="themes/dot.gif" class="icon ic_b_more">Trier par: </a>
+        <ul class="dropdown-menu dropdown-menu-start" aria-labelledby="navbarDropdown">
+            <li class="dropdown-item">
+                <a class="nav-link text-nowrap" href="{{ route('show.sort', ['sortBy' => 'titre']) }}">Titre</a>
+            </li>
+            <li class="dropdown-item">
+                <a class="nav-link text-nowrap" href="{{ route('show.sort', ['sortBy' => 'titre', 'order' => 'desc' ]) }}">Titre (Décroissant)</a>
+            </li>
+            <li class="dropdown-item">
+                <a class="nav-link text-nowrap" href="{{ route('show.sort', ['sortBy' => 'zip']) }}">Code postal</a>
+            </li> 
+            <li class="dropdown-item">
+                <a class="nav-link text-nowrap" href="{{ route('show.sort', ['sortBy' => 'zip', 'order' => 'desc']) }}">Code postal (Décroissant)</a>
+            </li>           
+            <li class="dropdown-item">
+                <a class="nav-link text-nowrap" href="{{ route('show.sort', ['sortBy' => 'prix']) }}">Prix</a>
+            </li>
+            <li class="dropdown-item">
+                <a class="nav-link text-nowrap" href="{{ route('show.sort', ['sortBy' => 'prix', 'order' => 'desc']) }}">Prix (Décroissant)</a>
+            </li>
+            <li class="dropdown-item">
+                <a class="nav-link text-nowrap" href="{{ route('show.sort', ['sortBy' => 'reservable']) }}">Réservable</a>
+            </li>
+            <li class="dropdown-item">
+                <a class="nav-link text-nowrap" href="{{ route('show.sort', ['sortBy' => 'reservable', 'order' => 'desc']) }}">Réservable (Décroissant)</a>
+            </li>
+        </ul>
+    </div>
     <ul>
-            @foreach($shows as $show)
-                <li><a href="{{ route('show.show', $show->id) }}">{{ $show->title }}</a></li>
-                @if($show->poster_url)
+        @foreach($shows as $show)
+            <li><a href="{{ route('show.show', $show->id) }}">{{ $show->title }}</a> {{-- {{ $show->location_id }} --}}</li>
+            @if($show->poster_url)
                 <li><a href="{{ route('show.show', $show->id) }}" style="width:200px; display:inline-block;"><img src="{{ asset('images/'.$show->poster_url) }}" alt="{{ $show->title }}" width="200"></a></li>
-                @else
+            @else
                 <li><a href="{{ route('show.show', $show->id) }}" style="width:200px; display:inline-block;"><canvas width="200" height="100" style="border:1px solid #000000;"></canvas></a></li>
-                @endif
-                    {{--@if($show->bookable)
-                        <span>{{ $show->price }}€</span>
-                    @endif
-
-                    @if($show->representations->count()==1)
-                    - <span>1 représentation</span>
-                    @elseif($show->representations->count()>1)
-                    - <span>{{ $show->representations->count() }} représentations</span>
-                    @else
-                    - <em>aucune représentation</em>
-                    @endif--}}
-            @endforeach
+            @endif
+        {{--@if($show->bookable)
+                <span>{{ $show->price }}€</span>
+            @endif
+            @if($show->representations->count()==1)
+                - <span>1 représentation</span>
+            @elseif($show->representations->count()>1)
+                - <span>{{ $show->representations->count() }} représentations</span>
+            @else
+                - <em>aucune représentation</em>
+            @endif--}}
+        @endforeach
     </ul>
 
     <div class="flex items-center">{{ $shows->links() }}</div>
